6f25c6aac376e17cf840a896596359582f61309c
anish <anishmjain2003@gmail.com>
2024-07-31 01:57:26 +0530

    done

--- app.py
+++ app.py
@@ -152,7 +152,8 @@ def predict():
             return render_template('index.html', error=f"An error occurred: {str(e)}")
 
     return render_template('index.html')
-
+   
+   
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(debug=True, host='0.0.0.0', port=port)
